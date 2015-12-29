bc24f2819b6aebbe53148a93e7f179f81d369741
Kirk Pinto <f1dude@gmail.com>
2015-12-28 21:58:47 -0800

    random updates

--- server/server.py
+++ server/server.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
 from flask import Flask, request
+from datetime import datetime
 app = Flask(__name__)
+
+@app.route('/sitrep', methods=['POST'])
+def getClientInfo():
+    uuid = request.form['uuid']
+    hostname = request.form['hostname']
+    internalIP = request.form['InternalIP']
+    externalIP = request.headers.getlist("X-Forwarded-For")[0]
+    lastCheckin = str(datetime.now())
+    return
 
 
 def serveClient():
